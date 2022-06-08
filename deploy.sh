4422d622ff85e772793006d5f957428926f7f505
dax <yasas.wijetilake@gmail.com>
2022-06-09 00:45:03 +0100

    updating yaml

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git init
 git add -A
 git commit -m "New Deployment"
 
-# git push -f git@github.com:yasasw/yasasw.github.io.git master
-git push -f git@github.com:yasasw/vue-demo-page.git master:gh-pages
+git push -f git@github.com:yasasw/yasasw.github.io.git master
+# git push -f git@github.com:yasasw/vue-demo-page.git master:gh-pages
 
 cd -
