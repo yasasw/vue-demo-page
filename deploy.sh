55d01d971096df1e7aeb47ddb2c3a82cec05525d
dax <yasas.wijetilake@gmail.com>
2022-06-09 20:58:39 +0100

    Updating package.json

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,15 @@ npm run build
 
 cd src/.vuepress/dist
 
+# cd .vuepress/dist
+# mv assets ./src/assets
+# cd src
+
 git init
 git add -A
 git commit -m "New Deployment"
 
-git push -f git@github.com:yasasw/yasasw.github.io.git master
-# git push -f git@github.com:yasasw/vue-demo-page.git master:gh-pages
+# git push -f git@github.com:yasasw/yasasw.github.io.git master
+git push -f git@ssh.github.com:yasasw/vue-demo-page.git master:gh-pages
 
 cd -
