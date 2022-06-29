1548aa150a68f30ec48bd22ee0098cd8411e3a75
dax <yasas.wijetilake@gmail.com>
2022-06-29 01:29:00 +0100

    Updating actions

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:yasasw/vue-demo-page.git
+git clone https://github.com/yasasw/vue-demo-page.git
 Username: yasasw
-Password: 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
+Password: $1
 
 cd vue-demo-page
 
@@ -13,6 +13,6 @@ wget https://raw.githubusercontent.com/$2/$3/$4/README.md -O $3.md
 git add $3.md
 git commit -m "New file $3"
 
-git push https://github.com/yasasw/myrepo.git
+git push https://github.com/yasasw/vue-demo-page.git
 Username: yasasw
-Password: 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
+Password: $1
