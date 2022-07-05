efb842cb8ef03cce6464b0a7d7789dd7b67c537a
dax <yasas.wijetilake@gmail.com>
2022-07-05 11:39:30 +0100

    Updating

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 echo running 
 
+git config --global user.email "yasas.wijetilake@gmail.com"
+git config --global user.name "yasasw"
+
 git clone https://github.com/yasasw/vue-demo-page.git
 Username: yasasw
 Password: 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
