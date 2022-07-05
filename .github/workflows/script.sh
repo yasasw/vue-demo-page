0bc9cc0821708500f4d946b8cccbc39e05de54f7
dax <yasas.wijetilake@gmail.com>
2022-07-05 12:00:44 +0100

    Updating

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
-echo running 
+echo running
 
 git config --global user.email "yasas.wijetilake@gmail.com"
 git config --global user.name "yasasw"
 
 git clone https://github.com/yasasw/vue-demo-page.git
-Username: yasasw
-Password: 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
 
 cd vue-demo-page
 
@@ -18,5 +16,3 @@ git add $3.md
 git commit -m "New file $3"
 
 git push https://github.com/yasasw/vue-demo-page.git
-Username: yasasw
-Password: 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
