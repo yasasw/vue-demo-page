f9a1bcbc4ef949c4249189f8a1e057ae3f223270
dax <yasas.wijetilake@gmail.com>
2022-07-05 11:37:40 +0100

    Updating

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 echo running 
 
-# git clone https://github.com/yasasw/vue-demo-page.git
-# Username: yasasw
-# Password: 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
+git clone https://github.com/yasasw/vue-demo-page.git
+Username: yasasw
+Password: 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
 
-# cd vue-demo-page
+cd vue-demo-page
 
-# cd src/plugins
+cd src/plugins
 
-# wget https://raw.githubusercontent.com/$2/$3/$4/README.md -O $3.md
+wget https://raw.githubusercontent.com/$2/$3/$4/README.md -O $3.md
 
-# git add $3.md
-# git commit -m "New file $3"
+git add $3.md
+git commit -m "New file $3"
 
-# git push https://github.com/yasasw/vue-demo-page.git
-# Username: yasasw
-# Password: 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
+git push https://github.com/yasasw/vue-demo-page.git
+Username: yasasw
+Password: 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
